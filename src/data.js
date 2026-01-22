6cc3d6c56c19f6e3a9813cdaf28359afc281d13a
Luka Popovic <lukapopovic0111@gmail.com>
2026-01-22 22:05:12 +0100

    Add form fields and income/expense categories config

---
+++ src/data.js
@@ -0,0 +1,104 @@
+export const formFields = {
+  incomeFields: [
+    {
+      type: "text",
+      text: "Salary Title",
+      variation: "standard",
+      value: "",
+    },
+    {
+      type: "text",
+      text: "Salary Amount",
+      variation: "standard",
+      value: "",
+    },
+    {
+      type: "date",
+      text: "Enter A Date",
+      variation: "standard",
+      value: "",
+    },
+  ],
+  expenseFields: [
+    {
+      type: "text",
+      text: "Expense Title",
+      variation: "standard",
+      value: "",
+    },
+    {
+      type: "text",
+      text: "Expense Title",
+      variation: "standard",
+      value: "",
+    },
+    {
+      type: "date",
+      text: "Enter A Date",
+      variation: "standard",
+      value: "",
+    },
+  ],
+};
+export const categories = {
+  incomeCategories: [
+    {
+      value: "salary",
+      text: "Salary",
+    },
+    {
+      value: "freelancing",
+      text: "Freelancing",
+    },
+    {
+      value: "investments",
+      text: "Investments",
+    },
+    {
+      value: "stocks",
+      text: "Stocks",
+    },
+    {
+      value: "bitcoin",
+      text: "bitcoin",
+    },
+    {
+      value: "Youtube",
+      text: "youtube",
+    },
+    {
+      value: "other",
+      text: "Other",
+    },
+  ],
+  expenseCategories: [
+    {
+      value: "education",
+      text: "Education",
+    },
+    {
+      value: "health",
+      text: "Health",
+    },
+    {
+      value: "clothing",
+      text: "Clothing",
+    },
+    {
+      value: "travelling",
+      text: "Travelling",
+    },
+    {
+      value: "groceries",
+      text: "Groceries",
+    },
+    {
+      value: "takeaways",
+      text: "Takeaways",
+    },
+    {
+      value: "other",
+      text: "Other",
+    },
+  ],
+};
