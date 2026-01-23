ad3af8b5f3f3bfa9a9ad0c148a2dc07456fe6da8
Luka Popovic <lukapopovic0111@gmail.com>
2026-01-23 21:24:34 +0100

    Add Option component

--- src/Option.jsx
+++ src/Option.jsx
@@ -1,1 +1,11 @@
-
+const Option = ({ optValue, children }) => {
+  return (
+    <option
+      value={optValue}
+      hidden={children === "Select Option" ? true : false}
+    >
+      {children}
+    </option>
+  );
+};
+export default Option;
