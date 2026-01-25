c295289cc87e6250f232e4ca40ddf6b3158eb09b
Luka Popovic <lukapopovic0111@gmail.com>
2026-01-25 14:38:26 +0100

    add styles for transactions list and summary boxes

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,8 +1,8 @@
 const Button = ({ btnType, clickAction, children, variation }) => {
   const baseClass = "btn";
   let btnClass;
-  if (variation === "standard-green") btnClass = "btn-standard-green";
-  if (variation === "standard-red") btnClass = "btn-standard-red";
+  if (variation === "standard") btnClass = "btn-standard";
+
   return (
     <button type={btnType} className={`${baseClass} ${btnClass}`}>
       {children}
